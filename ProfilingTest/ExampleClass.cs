207060b8ff0329113cf4d32bfe879123931492ae
igholm <ian.g.holm@intel.com>
2020-11-16 10:48:55 -0800

    Initial commit

---
+++ ProfilingTest/ExampleClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace ProfilingTest
+{
+    public class ExampleClass
+    {
+        public void Sleep100ms()
+        {
+            Thread.Sleep(100);
+        }
+
+        public void Delay100ms()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            while(stopwatch.ElapsedMilliseconds < 100) { }
+        }
+    }
+}
---
+++ ProfilingTest/ExampleTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace ProfilingTest
+{
+    [TestClass]
+    public class ExampleTests
+    {
+        private ExampleClass target;
+
+        public ExampleTests()
+        {
+            target = new ExampleClass();
+        }
+
+        [TestMethod]
+        public void Sleep100ms_SleepsAtLeast100Ms()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            target.Sleep100ms();
+            stopwatch.Stop();
+
+            Assert.IsTrue(stopwatch.ElapsedMilliseconds >= 100, $"Expected elapsed ms to be greater than or equal to 100 but was {stopwatch.ElapsedMilliseconds}");
+        }
+
+        [TestMethod]
+        public void Delay100ms_DelaysCorrectTime()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            target.Delay100ms();
+            stopwatch.Stop();
+
+            Assert.IsTrue(stopwatch.ElapsedMilliseconds >= 100, $"Expected elapsed ms to be greater than or equal to 100 but was {stopwatch.ElapsedMilliseconds}");
+        }
+    }
+}
